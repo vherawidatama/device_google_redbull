eb3352759c13bfaea7ad04b4b831a8d687f3b8ea
vherawidatama <vherawidatama@gmail.com>
2023-07-12 17:42:57 +0000

    first commit common for build derpfest

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -30,7 +30,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/derp/config/device_framework_matrix.xml
 
 # Needed for camera
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
